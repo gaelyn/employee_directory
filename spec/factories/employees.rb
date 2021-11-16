8cfb1853fec51ee8b22d9e84e529e53c1cd4c65e
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-11-16 08:45:15 -0600

    Update factory; delete model test

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :employee do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    age { rand(20.80) }
+    age { rand(20..80) }
   end
 end
